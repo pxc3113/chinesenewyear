e582297bed4a435d0c9ac79c3d29120d5717d047
pxc3110 <359342271@qq.com>
2022-01-22 17:31:33 +0800

    created by springboot initializr

---
+++ src/main/java/com/ytc/chinesenewyear/ChinesenewyearApplication.java
@@ -0,0 +1,13 @@
+package com.ytc.chinesenewyear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinesenewyearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinesenewyearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ytc/chinesenewyear/ChinesenewyearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytc.chinesenewyear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinesenewyearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
